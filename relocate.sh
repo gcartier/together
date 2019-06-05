4d0940f266a365d300580a63f133fffb19c709b5
Guillaume Cartier <gucartier@gmail.com>
2019-06-04 20:05:46 -0400

    Relocate script copy to jazz

--- relocate.sh
+++ relocate.sh
@@ -1,12 +1,14 @@
 #! /bin/sh
 
 GSTREAMER=/Library/Frameworks/GStreamer.framework/Versions/1.0
-DEST=cache/devel/gstreamer
+LIB=jazz/lib/jazz.stream/foreign/mac/gstreamer/gstreamer
+DEST=cache/develop/Libraries/gstreamer
 
 testlib() {
-    cp $GSTREAMER/$1 $DEST/$1
-    cache/devel/jazz -r relocate $DEST $1 /Library/Frameworks/GStreamer.framework/Versions/1.0 @rpath/gstreamer
+    cp $GSTREAMER/$1 $LIB/$1
+    cache/develop/jazz -r relocate $LIB $1 /Library/Frameworks/GStreamer.framework/Versions/1.0 @rpath/gstreamer
+    cp $LIB/$1 $DEST/$1
 }
 
-testlib lib/gstreamer-1.0/libgstaudiorate.so
-testlib lib/gstreamer-1.0/libgstwavenc.so
+testlib lib/gstreamer-1.0/libgstaudiorate.dylib
+testlib lib/gstreamer-1.0/libgstwavenc.dylib
